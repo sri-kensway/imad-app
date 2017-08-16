2b95dd712928d9e62d78bf0bc370fba480f92bc5
sri-kensway <30914593+sri-kensway@users.noreply.github.com>
2017-08-16 06:22:43 -0700

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/artical-one',function (req,res) {
-    res.send('Artical one requested and will be served here');
+    res.sendFile(path.join(_dirname,'ui','article-one.html'));
 });
 
 app.get('/artical-two',function (req,res) {
