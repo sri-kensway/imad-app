fd526a20cbcc1c559069d73fb3f2c6371a73ac74
sri-kensway <30914593+sri-kensway@users.noreply.github.com>
2017-08-16 10:31:54 -0700

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -50,10 +50,10 @@ var articles ={
 };
 
 function createTemplate (data){
-var title=data.title;
-var date=data.date;
-var heading=data.heading;
-var content=data.content;
+var title = data.title;
+var date = data.date;
+var heading = data.heading;
+var content = data.content;
 
 var htmlTemplate = `
 <html>
