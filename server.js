bce4621c3348fba6d57762828fc570a3053ea477
sri-kensway <30914593+sri-kensway@users.noreply.github.com>
2017-08-30 14:05:43 -0700

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -90,6 +90,10 @@ return htmlTemplate;
 
 }
 
+app.get('/counter',function (req,res){
+  counter=counter+1;
+  res.send(counter.toString());
+});
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
