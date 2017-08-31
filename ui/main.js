75262fcbbe7106ef5293c279ea29549294a06fe6
sri-kensway <30914593+sri-kensway@users.noreply.github.com>
2017-08-30 20:52:58 -0700

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -29,3 +29,20 @@ button.onclick= function () {
     
 };
 
+//Capture Submit values
+
+var nameInput=document.getElementById('name');
+var name=nameInput.value;
+var submit=document.getElementById('submit_btn');
+submit.oncick = function() {
+var names=['name1','name2','name3'];
+var list='';
+for (var i=0; i <names.length; i++)
+{
+ list += '<li>'+names[i]+'</li>'   ;
+    
+}
+
+var ui=document.getElementById('namelist');
+ui.innerHTML=list;
+};
