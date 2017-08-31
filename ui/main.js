f6ae4e4c59ca7382d52dc7050b90effe2d865050
sri-kensway <30914593+sri-kensway@users.noreply.github.com>
2017-08-31 14:12:24 -0400

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -35,14 +35,38 @@ var nameInput=document.getElementById('name');
 var name=nameInput.value;
 var submit=document.getElementById('submit-btn');
 submit.onclick = function() {
-var names=['name1','name2','name3','name4'];
-var list='';
-for (var i=0; i <names.length; i++)
-{
- list += '<li>'+names[i]+'</li>'   ;
     
-}
+     var request = new XMLHttpRequest();
+    //capture endpoint result and store in variable
+     request.onreadystatechange = function() {
+         if (request.readyState == XMLHttpRequest.DONE)
+           {
+              // do something
+              if (request.status == 200)
+                {
+                 //capture list of names 
+    
+                    var names=['name1','name2','name3','name4'];
+                    var list='';
+                    for (var i=0; i <names.length; i++)
+                            {
+                            list += '<li>'+names[i]+'</li>'   ;
+    
+                            }
 
 var ul=document.getElementById('namelist');
 ul.innerHTML=list;
+                }
+           }
+         //nothing
+     };
+     
+    //assign result to count variable and return count 
+    
+    //Make a requst object
+    
+    request.open ('GET', 'http://srikensway.imad.hasura-app.io/counter',true);
+    request.send(null);
+   
+  
 };
