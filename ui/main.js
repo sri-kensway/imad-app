4b75154f0947458a80be5ccc84ccf872a11104fe
sri-kensway <30914593+sri-kensway@users.noreply.github.com>
2017-08-30 19:50:31 -0700

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -13,7 +13,7 @@ button.onclick= function () {
                 {
                     var counter = request.responseText;
                      var span=document.getElementById('count');
-                    span.innerHTML=counter;
+                    span.innerHTML=counter.toString();
                 }
            }
          //nothing
