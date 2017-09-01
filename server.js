e2bc3165f0a735d15dc24b435781cc0bcde3946e
sri-kensway <30914593+sri-kensway@users.noreply.github.com>
2017-09-01 01:22:46 -0400

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var Pool = require('pg').Pool;
 var config = {
             user:'srikensway'  ,  
             databasename: 'srikensway',
-            host: 'http://db.imad.hasura-app.io',
+            host: 'db.imad.hasura-app.io',
             port: '5432',
             password: process.env.DB_PASSWORD
             };
